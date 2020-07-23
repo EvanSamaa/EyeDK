d2f9e2f57aba0294ecc7057a8929b83e20ea5deb
evan pan <evan12345pan@hotmail.com>
2020-07-23 12:09:43 -0400

    e

---
+++ main.py
@@ -0,0 +1,25 @@
+# You may need to restart your runtime prior to this, to let your installation take effect
+# Some basic setup:
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+# import some common libraries
+import numpy as np
+import cv2
+import random
+import matplotlib.pyplot as plt
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+import pandas as pd
+import numpy as np
+import os
+import tensorflow as tf
+from utils import *
+
+if __name__ == "__main__":
+    setup_logger()
+
---
+++ utils.py
@@ -0,0 +1,48 @@
+import cv2
+from scipy.spatial import distance
+import numpy as np
+################################ open cv functions ################################
+def mid_point(img, person, idx):
+    # get the coordinates
+    x1, y1, x2, y2 = person[idx]
+    _ = cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+    # compute bottom center of bbox
+    x_mid = int((x1 + x2) / 2)
+    y_mid = int(y2)
+    mid = (x_mid, y_mid)
+
+    _ = cv2.circle(img, mid, 5, (255, 0, 0), -1)
+    cv2.putText(img, str(idx), mid, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    return mid
+
+################################ Scipy functions ################################
+def compute_distance(midpoints,num):
+  dist = np.zeros((num,num))
+  for i in range(num):
+    for j in range(i+1,num):
+      if i!=j:
+        dst = distance.euclidean(midpoints[i], midpoints[j])
+        dist[i][j]=dst
+  return dist
+
+def find_closest(dist,num,thresh):
+  p1=[]
+  p2=[]
+  d=[]
+  for i in range(num):
+    for j in range(i,num):
+      if( (i!=j) & (dist[i][j]<=thresh)):
+        p1.append(i)
+        p2.append(j)
+        d.append(dist[i][j])
+  return p1,p2,d
+
+def change_2_red(img,person,p1,p2):
+  risky = np.unique(p1+p2)
+  points = []
+  for i in risky:
+    x1,y1,x2,y2 = person[i]
+    _ = cv2.rectangle(img, (x1, y1), (x2, y2), (0,0,255), 2)
+    points.append((int((x1+x2)/2), int(y2)))
+  return img, points
